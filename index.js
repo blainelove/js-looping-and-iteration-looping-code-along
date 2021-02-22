8d9c40b2a44f9d64e87c672bbdfffa54ce24ad9c
Blaine Love <blainelove94@gmail.com>
2021-02-21 22:42:09 -0500

    commit tree 1d904fbf1e1a002d679d8ddcdc258a38f1ff5191

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+writeCards["Ada", "Brendan", "Ali"];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+      console.log(`Thank you, ${names[i]} for the wonderful birthday gift`);
+    }
+  
+    return names;
+  }
+writeCards(names);
